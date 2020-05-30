b6354b7e14ec43c67dc5c1bba1135e88d09b3757
Alexander Ong <alexanderong@mail.weber.edu>
2020-05-29 20:02:55 -0600

    Fixed output text

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ new Vue({
       userWins: 0,
       compWins: 0,
       result: [
-        {message: ''}
+        {message: ''},
+        {player: false},
+        {comp: false},
+        {tie: false}
       ],
-      player: false,
-      comp: false,
-      tie: false, 
+      
   },
   methods: {
     pick: function (user){
@@ -26,63 +27,36 @@ new Vue({
       checkWinner() {
         if ((this.userChoice == 1 && this.compChoice == 3)) {
            this.userWins++
-           this.result.push({message: 'COMPUTER CHOSE SCISSORS | ROCK BEATS SCISSORS | PLAYER WINS!'})
-           this.player = true
-           this.comp = false
-           this.tie = false
+           this.result.push({message: 'COMPUTER CHOSE SCISSORS | ROCK BEATS SCISSORS | PLAYER WINS!', player: true})
         }
         else if  ((this.userChoice == 1 && this.compChoice == 2)){  
             this.compWins++
-            this.result.push({message: 'COMPUTER CHOSE PAPER | PAPER BEATS ROCK | COMPUTER WINS!'})
-            this.player = false
-            this.comp = true
-            this.tie = false
+            this.result.push({message: 'COMPUTER CHOSE PAPER | PAPER BEATS ROCK | COMPUTER WINS!', comp: true},)
         }
         else if  ((this.userChoice == 1 && this.compChoice == 1)){  
-          this.result.push({message: 'COMPUTER CHOSE ROCK | YOU HAVE TIED!'})
-          this.player = false
-          this.comp = false
-          this.tie = true
+          this.result.push({message: 'COMPUTER CHOSE ROCK | YOU HAVE TIED!', tie: true})
         }
         else if ((this.userChoice == 2 && this.compChoice == 1)) {
           this.userWins++
-          this.result.push({message: 'COMPUTER CHOSE ROCK | PAPER BEATS ROCK | PLAYER WINS!'})
-          this.player = true
-          this.comp = false
-          this.tie = false
+          this.result.push({message: 'COMPUTER CHOSE ROCK | PAPER BEATS ROCK | PLAYER WINS!',player: true})
        }
        else if  ((this.userChoice == 2 && this.compChoice == 3)){  
            this.compWins++
-           this.result.push({message: 'COMPUTER CHOSE SCISSORS | SCISSORS BEATS PAPER | COMPUTER WINS!'})
-           this.player = false
-           this.comp = true
-           this.tie = false
+           this.result.push({message: 'COMPUTER CHOSE SCISSORS | SCISSORS BEATS PAPER | COMPUTER WINS!',comp:true})
        }
        else if  ((this.userChoice == 2 && this.compChoice == 2)){  
-         this.result.push({message: 'COMPUTER CHOSE PAPER | YOU HAVE TIED!'})
-         this.player = false
-         this.comp = false
-         this.tie = true
+         this.result.push({message: 'COMPUTER CHOSE PAPER | YOU HAVE TIED!',tie:true})
        }
        else if ((this.userChoice == 3 && this.compChoice == 2)) {
         this.userWins++
-        this.result.push({message: 'COMPUTER CHOSE PAPER | SCISSORS BEATS PAPER | PLAYER WINS!'})
-        this.player = true
-        this.comp = false
-        this.tie = false
+        this.result.push({message: 'COMPUTER CHOSE PAPER | SCISSORS BEATS PAPER | PLAYER WINS!',player:true})
      }
      else if  ((this.userChoice == 3 && this.compChoice == 1)){  
          this.compWins++
-         this.result.push({message: 'COMPUTER CHOSE ROCK | ROCK BEATS SCISSORS | COMPUTER WINS!'})
-         this.player = false
-         this.comp = true
-         this.tie = false
+         this.result.push({message: 'COMPUTER CHOSE ROCK | ROCK BEATS SCISSORS | COMPUTER WINS!',comp:true})
      }
      else if  ((this.userChoice == 3 && this.compChoice == 3)){  
-       this.result.push({message: 'COMPUTER CHOSE SCISSORS | YOU HAVE TIED!'})
-       this.player = false
-       this.comp = false
-       this.tie = true
+       this.result.push({message: 'COMPUTER CHOSE SCISSORS | YOU HAVE TIED!',tie:true})
      }
        
         
