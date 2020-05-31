494296b891c0b553dfa13642e4d0499f162aa7a6
Alexander Ong <alexanderong@mail.weber.edu>
2020-05-30 23:50:26 -0600

    Added restart functionality now have to finish game end method

--- app.js
+++ app.js
@@ -12,55 +12,85 @@ new Vue({
         {comp: false},
         {tie: false}
       ],
+      userBar: 0,
+      compBar: 0
       
   },
   methods: {
     pick: function (user){
       this.userChoice = user,
       this.compChoice = Math.floor(Math.random() * 3 + 1)
+    },
+
+    restart: function () {
+      this.userWins = 0,
+      this.compChoice = -1,
+      this.userChoice = -1,
+      this.userWins = 0,
+      this.compWins = 0,
+      this.userBar = 0,
+      this.compBar = 0,
+      this.invis = true,
+      this.result = [
+        {message: ''},
+        {player: false},
+        {comp: false},
+        {tie: false}
+      ]
     }
   },
 
   computed: {
 
-   
+    updateUserWins() {
+      if(this.userWins > 0) {
+        this.userBar = 20 * this.userWins; 
+      }
+    },
+
+    updateCompWins() {
+      if(this.compWins > 0) {
+        this.compBar = 20 * this.compWins; 
+      }
+    },
+
       checkWinner() {
-        if ((this.userChoice == 1 && this.compChoice == 3)) {
+      if ((this.userChoice == 1 && this.compChoice == 3)) {
            this.userWins++
            this.result.push({message: 'COMPUTER CHOSE SCISSORS | ROCK BEATS SCISSORS | PLAYER WINS!', player: true})
-        }
-        else if  ((this.userChoice == 1 && this.compChoice == 2)){  
+      }
+      else if  ((this.userChoice == 1 && this.compChoice == 2)){  
             this.compWins++
             this.result.push({message: 'COMPUTER CHOSE PAPER | PAPER BEATS ROCK | COMPUTER WINS!', comp: true},)
-        }
-        else if  ((this.userChoice == 1 && this.compChoice == 1)){  
+      }
+      else if  ((this.userChoice == 1 && this.compChoice == 1)){  
           this.result.push({message: 'COMPUTER CHOSE ROCK | YOU HAVE TIED!', tie: true})
-        }
-        else if ((this.userChoice == 2 && this.compChoice == 1)) {
+      }
+      else if ((this.userChoice == 2 && this.compChoice == 1)) {
           this.userWins++
           this.result.push({message: 'COMPUTER CHOSE ROCK | PAPER BEATS ROCK | PLAYER WINS!',player: true})
-       }
-       else if  ((this.userChoice == 2 && this.compChoice == 3)){  
+      }
+      else if  ((this.userChoice == 2 && this.compChoice == 3)){  
            this.compWins++
            this.result.push({message: 'COMPUTER CHOSE SCISSORS | SCISSORS BEATS PAPER | COMPUTER WINS!',comp:true})
-       }
-       else if  ((this.userChoice == 2 && this.compChoice == 2)){  
+      }
+      else if  ((this.userChoice == 2 && this.compChoice == 2)){  
          this.result.push({message: 'COMPUTER CHOSE PAPER | YOU HAVE TIED!',tie:true})
-       }
-       else if ((this.userChoice == 3 && this.compChoice == 2)) {
+      }
+      else if ((this.userChoice == 3 && this.compChoice == 2)) {
         this.userWins++
         this.result.push({message: 'COMPUTER CHOSE PAPER | SCISSORS BEATS PAPER | PLAYER WINS!',player:true})
-     }
-     else if  ((this.userChoice == 3 && this.compChoice == 1)){  
+      }
+      else if  ((this.userChoice == 3 && this.compChoice == 1)){  
          this.compWins++
          this.result.push({message: 'COMPUTER CHOSE ROCK | ROCK BEATS SCISSORS | COMPUTER WINS!',comp:true})
-     }
-     else if  ((this.userChoice == 3 && this.compChoice == 3)){  
+      }
+      else if  ((this.userChoice == 3 && this.compChoice == 3)){  
        this.result.push({message: 'COMPUTER CHOSE SCISSORS | YOU HAVE TIED!',tie:true})
-     }
+      }
        
-        
-        
-      }  
+    },//end CheckWinner method 
+    
+    
   }
 });
